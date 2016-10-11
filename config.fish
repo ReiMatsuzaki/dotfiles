bc47ef8c3d5dbb1a11ae6c261c04ea03b33099ee
Matsuzaki Rei <matsuzakirei@Matsuzaki-no-MacBook-Pro.local>
2016-10-11 18:42:37 +0900

    write emacs and its client alias

--- config.fish
+++ config.fish
@@ -1,3 +1,7 @@
-# alias f='emacsclient -nw -a ""'
-alias f='emacs -nw'
+alias f='emacsclient -nw -a ""'
+#alias f='emacsclient -nw --socket-name /tmp/emacs501/server'
+#alias f='emacs -nw'
 alias kill-emacs='emacsclient -e "(kill-emacs)"'
+
+set -x LIBRARY_PATH ~/local/lib
+set -x CPLUS_INCLUDE_PATH ~/local/include/
